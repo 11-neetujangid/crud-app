a2042107a4746a2521f6a1a0eb77a7cbce32b7ec
11-neetujangid <64856080+11-neetujangid@users.noreply.github.com>
2021-08-10 10:43:01 +0530

    Update action.js

--- src/Actions/action.js
+++ src/Actions/action.js
@@ -5,23 +5,8 @@ export const EDIT_USERS = "EDIT_USERS";
 export const SIGNUP_USERS = "SIGNUP_USERS";
 
 
-
-
-
 export const setUser =(payload) =>({type: SET_USERS, payload:payload});
 export const editUser= (payload) =>({ type : EDIT_USERS, payload:payload});
 export const SignupUser= (payload) =>({ type : SIGNUP_USERS, payload:payload});
-
-const url = "http://localhost:3001/users";
-export const Signup = (user) =>{
-    return function (dispatch){
-        axios.post(`${url}`, user)
-        .then((res) =>{console.log(res);
-        dispatch(SignupUser());
-        })
-        .catch((err) => console.log(err))
-    }
-     
-}
 
 
